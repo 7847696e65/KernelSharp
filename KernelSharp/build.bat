c43d74b3f8bd191b361edc9b7b63149608f949ca
7847696e65 <96039216+7847696e65@users.noreply.github.com>
2025-06-08 18:32:18 +0800

    s
    s

--- KernelSharp/build.bat
+++ KernelSharp/build.bat
@@ -1,29 +1,44 @@
 @echo off
 
-:: you have to set the path to whatever it is for you
-@set ILCPATH="E:\tools"
-@set NTOSKRNLLIBPATH="D:\Windows Kits\10\lib\10.0.19041.0\km\x64\ntoskrnl.lib"
+:: Adjust these paths to your actual system setup
+set ILCPATH="C:\Users\TAMMY\.nuget\packages\runtime.win-x64.microsoft.dotnet.ilcompiler\7.0.0\tools"
+set NTOSKRNLLIBPATH="C:\Program Files (x86)\Windows Kits\10\Lib\10.0.19041.0\km\x64\ntoskrnl.lib"
 
-@if not exist %ILCPATH%\ilc.exe (
-  echo The DROPPATH environment variable not set.
-  exit /B
+if not exist %ILCPATH%\ilc.exe (
+  echo ILC not found at %ILCPATH%
+  exit /B 1
 )
 
-@del Program.ilexe >nul 2>&1
-@del Program.obj >nul 2>&1
-@del Program.exe >nul 2>&1
-@del Program.map >nul 2>&1
-@del Program.pdb >nul 2>&1
+del Program.ilexe >nul 2>&1
+del Program.obj >nul 2>&1
+del Program.exe >nul 2>&1
+del Program.map >nul 2>&1
+del Program.pdb >nul 2>&1
 
-@if "%1" == "clean" exit /B
+if "%1"=="clean" exit /B
 
-csc.exe /nologo /debug:embedded /noconfig /nostdlib /runtimemetadataversion:v4.0.30319 Program.cs WDK.cs Runtime/InteropServices.cs Runtime/CompilerHelpers.cs Runtime/CompilerServices.cs Runtime/System.cs Runtime/Runtime.cs /out:Program.ilexe /langversion:latest /unsafe || goto Error
-%ILCPATH%\ilc Program.ilexe -o Program.obj --systemmodule Program --map Program.map -O || goto Error
+:: Compile C# to .ilexe (IL)
+csc.exe /nologo /debug:embedded /noconfig /nostdlib /runtimemetadataversion:v4.0.30319 ^
+  Program.cs ^
+  Util.cs ^
+  NTImage.cs ^
+  WDK.cs ^
+  Runtime\InteropServices.cs ^
+  Runtime\CompilerHelpers.cs ^
+  Runtime\CompilerServices.cs ^
+  Runtime\System.cs ^
+  Runtime\Runtime.cs ^
+  /out:Program.ilexe /langversion:latest /unsafe || goto Error
 
-:: Also set this path to your oath
+:: Convert IL to OBJ via NativeAOT (ILC)
+%ILCPATH%\ilc.exe Program.ilexe -o Program.obj --systemmodule Program --map Program.map -O || goto Error
+
+:: Link OBJ to SYS
 link.exe %NTOSKRNLLIBPATH% /nologo /subsystem:native /DRIVER:WDM Program.obj /entry:DriverEntry /incremental:no /out:Driver.sys || goto Error
-@goto :EOF
+
+echo Build succeeded!
+goto :EOF
 
 :Error
-@echo Tool failed.
+echo Tool failed.
 exit /B 1
